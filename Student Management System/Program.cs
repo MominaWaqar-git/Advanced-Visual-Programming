a96b7858b3a38fc76b8e1da73e138aadf944cf68
Momina Waqar <mominawaqar30@gmail.com>
2026-02-27 15:17:14 +0500

    Add project files.

---
+++ Student Management System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Management_System
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
